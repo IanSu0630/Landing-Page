cc396176a74985be2b16da8fe7c8c78e451016b0
IanSu0630 <eph3k2625@gmail.com>
2020-03-07 23:19:15 +0800

    0307更新

---
+++ js/all.js
@@ -0,0 +1,47 @@
+// 註冊驗證
+var send = document.querySelector('.send');
+
+send.addEventListener('click',signup,false);
+
+function signup(){
+    var emailStr = document.querySelector('.account').value;
+    var passwordStr = document.querySelector('.password').value;
+    var passwordVeriStr = document.querySelector('.passwordVeri').value;
+
+    if(passwordStr != passwordVeriStr){
+        alert('確認密碼欄需與密碼相同！');
+    }else{
+        var account = {};
+    account.email = emailStr;
+    account.password = passwordStr;
+    
+    var xhr = new XMLHttpRequest();
+    xhr.open('post','https://hexschool-tutorial.herokuapp.com/api/signup',true);
+    xhr.setRequestHeader('Content-type','application/json');
+    var data = JSON.stringify(account);
+    xhr.send(data);
+    xhr.onload = function(){
+        var callbackData = JSON.parse(xhr.responseText);
+        console.log(callbackData);
+        var veriStr =  callbackData.message;
+        if(veriStr =="帳號註冊成功"){
+            alert('帳號註冊成功！！');
+        }else{
+            alert("帳號註冊失敗！");
+        }
+    }
+    }   
+}
+
+// 修正行動版 RegisterModal 切換後滾動問題
+$(document).ready(function () {
+    console.log('ready');
+    $('a[data-dismiss="modal"][data-toggle="modal"]').on('click', function () {
+        console.log('click');
+        var target = $(this).data('target');
+        console.log('target', target);
+        $(target).on('shown.bs.modal', function () {
+            $('body').addClass('modal-open');
+        });
+    });
+})
